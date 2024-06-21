0ca2420978cd53033edd175a53beb29086a04cc7
Minthantmg <minthantmg@gicjp.com>
2024-06-21 18:47:05 +0900

    add api logic

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,10 +1,59 @@
 import {db} from "@/db";
+import {NextRequest} from "next/server";
+import {ProductFilterValidator} from "@/lib/validators/product-filter-validator";
 
-export const POST = async () => {
-    const products = await db.query({
-        topK: 12,
-        vector: [0,0,0],
-        includeMetadata: true,
-    })
-    return new Response(JSON.stringify(products))
+class Filter {
+    private filters: Map<String, String[]> = new Map()
+
+    hasFilter() {
+        return this.filters.size > 0
+    }
+
+    add(key: string, operator: string, value: string | number) {
+        const filter = this.filters.get(key) || []
+        filter.push(`${key} ${operator} ${typeof value === "number" ? value : `"${value}"`}`)
+        this.filters.set(key, filter)
+    }
+
+    addRaw(key: string, rawFilter: string) {
+        this.filters.set(key, [rawFilter])
+    }
+
+    get() {
+        const parts: string[] = []
+        this.filters.forEach((filter) => {
+            const groupedValues = filter.join(` OR `)
+            parts.push(`(${groupedValues})`)
+        })
+        return parts.join(' AND ')
+    }
+}
+
+const AVG_PRODUCT_PRICE = 25
+const MAX_PRODUCT_PRICE = 50
+export const POST = async (req: NextRequest) => {
+    try {
+        const body = await req.json()
+
+        const {color, price, size, sort} = ProductFilterValidator.parse(body.filter)
+        const filter = new Filter()
+        color.forEach((color) => filter.add("color", "=", color))
+        size.forEach((size) => filter.add("size", "=", size))
+        filter.addRaw("price", `price >= ${price[0]} AND price <= ${price[1]}`)
+
+        const products = await db.query({
+            topK: 12,
+            vector: [0, 0, sort === "none" ? AVG_PRODUCT_PRICE : sort === "price-asc" ? 0 : MAX_PRODUCT_PRICE],
+            includeMetadata: true,
+            filter: filter.hasFilter() ? filter.get() : undefined
+        })
+        return new Response(JSON.stringify(products))
+    } catch (err) {
+        console.log(err)
+
+        return new Response(JSON.stringify({message: "Internal Server Error"}),
+            {
+                status: 500
+            })
+    }
 }
