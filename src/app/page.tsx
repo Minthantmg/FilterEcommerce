712a3d7bfdb6a5a8ebbf609b6a9a948dfafa207c
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-06-23 23:03:56 +0630

    fix:cors error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
         queryKey: ['products'],
         queryFn: async () => {
             const {data} = await axios.post<QueryResult<TProduct>[]>(
-                'http://localhost:3000/api/products',
+                '/api/products',
                 {
                     filter: {
                         sort: filter.sort,
